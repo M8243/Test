1f78ef33132692d1f6ab6d63f9dad1767c265722
M8243 <2577251420@qq.com>
2023-10-02 11:15:19 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #define I2C_SDA  5
 #define I2C_SCL  6
 
+TaskHandle_t taskHandle;
+
 void oledTask(void * pvParam) {
   U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* I2C_SCL=*/ I2C_SCL, /* I2C_SDA=*/ I2C_SDA);
   u8g2.begin();
@@ -19,11 +21,39 @@ void oledTask(void * pvParam) {
     u8g2.sendBuffer();					// transfer internal memory to the display
     vTaskDelay(1000);
   }
+  
 }
+void showStockTask(void *ptParam) {
+  static float stockPrice = 99.57; //股票价格
+  TickType_t xLastWakeTime = xTaskGetTickCount();
 
+  const TickType_t xFrequency = 3000; // 间隔 3000 ticks = 3 seconds
+
+  for (;;) {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+    Serial.println(xTaskGetTickCount());
+    stockPrice = stockPrice * (1 + random(1, 20) / 100.0);
+    // vTaskDelay(random(500, 2000));
+    Serial.print("Stock Price : $");
+    Serial.println(stockPrice);
+  }
+
+}
 void setup() { //loopBack , Priority 1, Core 1
-  xTaskCreatePinnedToCore(oledTask, "OLED Task", 1024 * 6, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(oledTask, "OLED Task", 1024 * 6, NULL, 1, &taskHandle, 1);
+  Serial.begin(115200);
+  
 
+  vTaskDelay(2000);
+  int waterMark = uxTaskGetStackHighWaterMark(taskHandle);
+  Serial.print("Task Free Memory: ");
+  Serial.print(waterMark);
+  Serial.println(" Bytes");
+  Serial.print("Task Used Memory: ");
+  Serial.print(1024*6 - waterMark);
+  Serial.println(" Bytes");
+
+  xTaskCreate(showStockTask, "Show Stock Price", 1024 * 6, NULL, 1, NULL);
   vTaskDelete(NULL);
 }
 
